84e0eb2c3c7991b1365fd4ad65ea7f2404409766
Takeo <alexandervilmar81@gmail.com>
2021-01-19 14:28:06 +0000

    init

---
+++ Assets/GunScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+
+    public GameObject Bullet;
+   
+    public GameObject bulletpoint;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            shoot();
+        }
+
+    }
+
+    void shoot()
+    {
+        GameObject clone = Instantiate(Bullet, bulletpoint.transform.position, bulletpoint.transform.rotation);
+        clone.GetComponent<Rigidbody2D>().AddForce(bulletpoint.transform.right * speed) ;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float MoveSpeed = 10f;
+    public float jumpForce = 5f;
+
+    public Rigidbody2D playerRB;
+
+    public bool isGrounded = false;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float X = Input.GetAxisRaw("Horizontal");
+
+        playerRB.velocity = new Vector2(X * MoveSpeed, playerRB.velocity.y);
+
+
+        if (X <= -0.1)
+        {
+            gameObject.transform.eulerAngles = new Vector3(0,180,0);
+        }
+        if (X >= 0.1)
+        {
+            gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded == true)
+        {
+            playerRB.AddForce(Vector2.up * jumpForce);
+            isGrounded = false;
+            
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
